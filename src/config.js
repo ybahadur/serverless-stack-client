7b29738657de31b205c8e3bb31171fe07a83684c
Yusuf Bahadar <yusuf.bahadar@Yusuf-MBPi.local>
2020-06-24 17:19:38 -0700

    Setting up our react app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "notes-yusuf-bucket"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://4b9f5gylf2.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_d9816wxQl",
+    APP_CLIENT_ID: "2dv9kjitd9mkdi0h4e3bdo4m96",
+    IDENTITY_POOL_ID: "us-east-2:c1e5c587-4b95-46bd-ac9e-07dfdb41f987"
+  }
+};
